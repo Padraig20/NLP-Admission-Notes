d60cb322398a3d0afc7a6773899e73e3b524817e
M00nD3v <26168940+M00nD3v@users.noreply.github.com>
2023-07-12 22:27:53 +0200

    moved fronted folder to frontend-legacy

---
+++ frontend-legacy/src/app/app.component.spec.ts
@@ -0,0 +1,29 @@
+import { TestBed, waitForAsync } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+
+describe('AppComponent', () => {
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule, HttpClientTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'sepm-group-phase'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('SEPM Group Phase');
+  });
+});
